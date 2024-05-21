fc7f5b12ea2ea3e6129bf9faa707ade6287ad537
mahesh dash <maheshdash84821@gmail.com>
2024-05-21 13:47:33 +0530

    Path change for index file

--- Djangoseries/Djangoseries/views.py
+++ Djangoseries/Djangoseries/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello Mahesh you are in Home Page")
-    return render(request,'index.html')
+    return render(request,'website/index.html')
     
 def about(request):
     return HttpResponse("Hello Mahesh you are in About Page")
